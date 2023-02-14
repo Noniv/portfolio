da1b7bd4f32bfe6c69fc1eea3131263e7cc5cf54
Maksymilian Dendura <maksd0410@gmail.com>
2023-02-15 00:19:44 +0100

    Added google SEO meta tag

--- app/head.js
+++ app/head.js
@@ -4,6 +4,7 @@ export default function Head() {
       <title>Maksymilian Dendura - Front End Developer</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Hi, my name is Maksymilian Dendura and I am a front end developer." />
+      <meta name="google-site-verification" content="TaASkcxwXDFTFjIfoDuT-GU0tomLD6ohjcPmVW2Ed5k" />
       <link rel="icon" href="/logo.webp" />
     </>
   )
