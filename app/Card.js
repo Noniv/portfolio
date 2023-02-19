51e810153d0571e67f3bebc01d6ea05c63cc311b
Maksymilian Dendura <maksd0410@gmail.com>
2023-02-19 19:34:22 +0100

    Added better image scaling for card images

--- app/Card.js
+++ app/Card.js
@@ -9,13 +9,15 @@ export default function Card({ src, title, description, children, href }) {
       target="_blank"
     >
       <div className="text-center shadow-lg pb-10 rounded-xl overflow-hidden dark:bg-slate-800 h-full mb-10">
+        <div className="md:h-48 overflow-hidden">
         <Image
           src={src}
-          height={950}
-          width={1900}
-          className="w-full max-h-48"
+          height={953}
+          width={1920}
+          className="md:min-h-full"
           alt="Website photo"
         />
+        </div>
         <h4 className="text-lg font-medium pt-8 pb-2 text-teal-600">{title}</h4>
         <div className="px-10">{description}</div>
         <div className="flex gap-6 justify-center text-2xl pt-6">
